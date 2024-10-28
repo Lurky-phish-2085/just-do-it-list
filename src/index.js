95f3ab55c4f8764cb0987258a667d39ec5317877
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-10-28 13:58:39 +0800

    Implement finished items list feature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const itemNameInput = document.querySelector("#item-name-input");
 const addItemButton = document.querySelector("#add-item-button");
 const listContainer = document.querySelector("#list-container");
+const finishedListContainer = document.querySelector(
+  "#finished-list-container"
+);
 
 const items = [];
+const finishedItems = [];
 updateList();
 
 const addListItem = () => {
@@ -28,8 +32,9 @@ function addItem(itemName) {
 
 function updateList() {
   listContainer.replaceChildren();
+  finishedListContainer.replaceChildren();
 
-  if (items.length === 0) {
+  if (items.length === 0 && finishedItems.length === 0) {
     const listEmptyMessage = document.createElement("p");
     listEmptyMessage.textContent = "To-Do List is empty...";
     listContainer.appendChild(listEmptyMessage);
@@ -37,7 +42,7 @@ function updateList() {
     return;
   }
 
-  for (let index = 0; index < items.length; index++) {
+  for (let index = items.length - 1; index >= 0; index--) {
     const value = items[index];
 
     const listItemContainer = document.createElement("article");
@@ -53,13 +58,44 @@ function updateList() {
     listContainer.appendChild(listItemContainer);
 
     checkButton.addEventListener("click", () => {
-      items.splice(index, 1);
+      finishedItems.push(items.splice(index, 1));
+      itemNameInput.focus();
       updateList();
     });
     deleteButton.addEventListener("click", () => {
       items.splice(index, 1);
+      itemNameInput.focus();
       updateList();
     });
-    itemValue.textContent = `${index}: ${value}`;
+    itemValue.textContent = value;
+  }
+
+  for (let index = finishedItems.length - 1; index >= 0; index--) {
+    const value = finishedItems[index];
+
+    const listItemContainer = document.createElement("article");
+    const undoButton = document.createElement("button");
+    undoButton.textContent = "\u{21B6}";
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "\u{2715}";
+    const itemValue = document.createElement("span");
+    itemValue.style.textDecoration = "line-through";
+
+    listItemContainer.appendChild(undoButton);
+    listItemContainer.appendChild(deleteButton);
+    listItemContainer.appendChild(itemValue);
+    finishedListContainer.appendChild(listItemContainer);
+
+    undoButton.addEventListener("click", () => {
+      items.push(finishedItems.splice(index, 1));
+      itemNameInput.focus();
+      updateList();
+    });
+    deleteButton.addEventListener("click", () => {
+      finishedItems.splice(index, 1);
+      itemNameInput.focus();
+      updateList();
+    });
+    itemValue.textContent = value;
   }
 }
