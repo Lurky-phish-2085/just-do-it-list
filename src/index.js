a8beddadf6aed09f2d20fa39418744f2606a3289
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-10-30 11:41:05 +0800

    Refactor codebase to utilize the Task model
    - Refactor Task item saving logic utilizing Task model
    - Refactor list view update to use the Task model instead

--- src/index.js
+++ src/index.js
@@ -1,112 +1,3 @@
-const itemNameInput = document.querySelector("#item-name-input");
-const addItemButton = document.querySelector("#add-item-button");
-const listContainer = document.querySelector("#list-container");
-const itemNameInputError = document.querySelector("#item-name-input-error");
-const finishedListContainer = document.querySelector(
-  "#finished-list-container"
-);
-
-const items = [];
-const finishedItems = [];
-updateList();
-
-const addListItem = () => {
-  if (itemNameInput.value === "") {
-    itemNameInputError.classList.remove("hidden");
-    return;
-  }
-
-  itemNameInputError.classList.add("hidden");
-  addItem(itemNameInput.value);
-  itemNameInput.value = "";
-  setTimeout(() => itemNameInput.focus(), 80); // prevents the itemNameInput from firing any event.
-};
-addItemButton.addEventListener("click", addListItem);
-itemNameInput.addEventListener("keyup", (event) => {
-  if (event.key !== "Enter") {
-    return;
-  }
-
-  addListItem();
-});
-
-function addItem(itemName) {
-  items.push(itemName);
-  updateList();
-}
-
-function updateList() {
-  listContainer.replaceChildren();
-  finishedListContainer.replaceChildren();
-
-  if (items.length === 0 && finishedItems.length === 0) {
-    const listEmptyMessage = document.createElement("p");
-    listEmptyMessage.textContent = "To-Do List is empty...";
-    listContainer.appendChild(listEmptyMessage);
-
-    return;
-  }
-
-  for (let index = items.length - 1; index >= 0; index--) {
-    const value = items[index];
-
-    const listItemContainer = document.createElement("article");
-    const checkButton = document.createElement("button");
-    checkButton.textContent = "\u{2713}";
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "\u{2715}";
-    const itemValue = document.createElement("span");
-
-    listItemContainer.appendChild(checkButton);
-    listItemContainer.appendChild(deleteButton);
-    listItemContainer.appendChild(itemValue);
-    listItemContainer.classList.add("list-item");
-    listContainer.appendChild(listItemContainer);
-
-    checkButton.addEventListener("click", () => {
-      finishedItems.push(items.splice(index, 1));
-      itemNameInput.focus();
-      updateList();
-    });
-    deleteButton.addEventListener("click", () => {
-      items.splice(index, 1);
-      itemNameInput.focus();
-      updateList();
-    });
-    itemValue.textContent = value;
-  }
-
-  for (let index = finishedItems.length - 1; index >= 0; index--) {
-    const value = finishedItems[index];
-
-    const listItemContainer = document.createElement("article");
-    const undoButton = document.createElement("button");
-    undoButton.textContent = "\u{21B6}";
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "\u{2715}";
-    const itemValue = document.createElement("span");
-    itemValue.style.textDecoration = "line-through";
-
-    listItemContainer.appendChild(undoButton);
-    listItemContainer.appendChild(deleteButton);
-    listItemContainer.appendChild(itemValue);
-    listItemContainer.classList.add("list-item");
-    finishedListContainer.appendChild(listItemContainer);
-
-    undoButton.addEventListener("click", () => {
-      items.push(finishedItems.splice(index, 1));
-      itemNameInput.focus();
-      updateList();
-    });
-    deleteButton.addEventListener("click", () => {
-      finishedItems.splice(index, 1);
-      itemNameInput.focus();
-      updateList();
-    });
-    itemValue.textContent = value;
-  }
-}
-
 class Task {
   static db = [];
 
@@ -134,6 +25,14 @@ class Task {
     return this.db.splice(this.#findIndexOf(id), 1);
   }
 
+  static size() {
+    return this.db.length;
+  }
+
+  static isEmpty() {
+    return this.db.length === 0;
+  }
+
   save() {
     const newTask = JSON.parse(JSON.stringify(this));
     newTask.id = Task.findAll().length + 1;
@@ -150,5 +49,94 @@ class Task {
     });
 
     return elementIndex;
+  }
+}
+
+const itemNameInput = document.querySelector("#item-name-input");
+const addItemButton = document.querySelector("#add-item-button");
+const listContainer = document.querySelector("#list-container");
+const itemNameInputError = document.querySelector("#item-name-input-error");
+const finishedListContainer = document.querySelector(
+  "#finished-list-container"
+);
+
+// const items = [];
+// const finishedItems = [];
+updateList();
+
+const addListItem = () => {
+  if (itemNameInput.value === "") {
+    itemNameInputError.classList.remove("hidden");
+    return;
+  }
+
+  itemNameInputError.classList.add("hidden");
+  addItem(itemNameInput.value);
+  itemNameInput.value = "";
+  setTimeout(() => itemNameInput.focus(), 80); // prevents the itemNameInput from firing any event.
+};
+addItemButton.addEventListener("click", addListItem);
+itemNameInput.addEventListener("keyup", (event) => {
+  if (event.key !== "Enter") {
+    return;
+  }
+
+  addListItem();
+});
+
+function addItem(itemName) {
+  const task = new Task(itemName, false);
+  task.save();
+  updateList();
+}
+
+function updateList() {
+  listContainer.replaceChildren();
+  finishedListContainer.replaceChildren();
+
+  if (Task.isEmpty()) {
+    const listEmptyMessage = document.createElement("p");
+    listEmptyMessage.textContent = "To-Do List is empty...";
+    listContainer.appendChild(listEmptyMessage);
+
+    return;
+  }
+
+  const CHECK_ICON = "\u{2713}";
+  const DELETE_ICON = "\u{2715}";
+  const UNDO_ICON = "'\u{21B6}";
+
+  const items = Task.findAll();
+  for (let index = items.length - 1; index >= 0; index--) {
+    const item = items[index];
+
+    const listItemContainer = document.createElement("article");
+    const actionButton = document.createElement("button");
+    actionButton.textContent = item.done ? UNDO_ICON : CHECK_ICON;
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = DELETE_ICON;
+    const itemValue = document.createElement("span");
+
+    listItemContainer.appendChild(actionButton);
+    listItemContainer.appendChild(deleteButton);
+    listItemContainer.appendChild(itemValue);
+    listItemContainer.classList.add("list-item");
+
+    if (item.done) {
+      itemValue.style.textDecoration = "line-through";
+      finishedListContainer.append(listItemContainer);
+    } else {
+      listContainer.appendChild(listItemContainer);
+    }
+
+    itemValue.textContent = item.description;
+    actionButton.addEventListener("click", () => {
+      Task.update(item.id, { ...item, done: !item.done });
+      updateList();
+    });
+    deleteButton.addEventListener("click", () => {
+      Task.delete(item.id);
+      updateList();
+    });
   }
 }
