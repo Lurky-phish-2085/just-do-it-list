9871590529c1c09b6c246602a61b454ad2e4a651
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-11-24 16:56:10 +0800

    Implement TaskRepository updates observer pattern

--- src/data/TaskRepository.ts
+++ src/data/TaskRepository.ts
@@ -3,6 +3,8 @@ import TaskModel from "./TaskModel";
 const TASK_REPOSITORY_ITEM = "tasks";
 
 class TaskRepository {
+  private static listeners: Array<Function> = [];
+
   public static instance(): TaskModel[] {
     const tasks: TaskModel[] = JSON.parse(
       localStorage.getItem(TASK_REPOSITORY_ITEM) || "[]"
@@ -11,23 +13,44 @@ class TaskRepository {
     return tasks;
   }
 
-  public static update(db: TaskModel[]): void {
-    localStorage.setItem(TASK_REPOSITORY_ITEM, JSON.stringify(db));
-  }
-
-  public static initialize(initialData: TaskModel[] = []): void {
+  public static initialize(data: TaskModel[] = []): void {
     const repoExist = localStorage.getItem(TASK_REPOSITORY_ITEM);
 
     if (repoExist) {
       return;
     }
 
-    const data = initialData.map((task, index) => {
+    const initialData = data.map((task, index) => {
       task.id = index + 1;
       return task;
     });
 
-    localStorage.setItem(TASK_REPOSITORY_ITEM, JSON.stringify(data));
+    this.update(initialData);
+  }
+
+  public static update(db: TaskModel[]): void {
+    localStorage.setItem(TASK_REPOSITORY_ITEM, JSON.stringify(db));
+    this.notify();
+  }
+
+  public static addListener(listener: () => void) {
+    this.listeners.push(listener);
+    console.log("listeners size: " + this.listeners.length);
+  }
+
+  public static removeListener(listener: () => void) {
+    this.listeners.splice(this.listeners.indexOf(listener), 1);
+    console.log("listeners size: " + this.listeners.length);
+  }
+
+  private static notify() {
+    if (this.listeners.length === 0) {
+      return;
+    }
+
+    for (const listener of this.listeners) {
+      listener();
+    }
   }
 }
 
